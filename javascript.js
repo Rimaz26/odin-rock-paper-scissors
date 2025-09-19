199dfba9d0482f853cc6381de76c6bb3b2199462
Rimaz <mohommedrimaz26@gmail.com>
2025-09-19 15:46:22 +0530

    Done upto 5th step

--- javascript.js
+++ javascript.js
@@ -1,23 +1,61 @@
-let numberRandom = Math.random();
-let compChoice;
-let userChoice;
+let humanScore = 0;
+let computerScore = 0;
 
-if(numberRandom >= 0 && numberRandom <= 0.3)
-{
-    compChoice = "Rock";
+function getComputerChoice() {
+
+    let numberRandom = Math.random();
+    let compChoice;
+
+    if (numberRandom >= 0 && numberRandom <= 0.3) {
+       return compChoice = "rock";
+    }
+
+    else if (numberRandom >= 0.3 && numberRandom <= 0.6) {
+       return compChoice = "paper";
+    }
+
+    else {
+      return  compChoice = "scissor";
+    }
+
+    //return console.log(numberRandom) ;
 }
 
-else if( numberRandom >= 0.3 && numberRandom <= 0.6)
-{
- compChoice = "Paper";
+function getHumanChoice() {
+
+    let humanChoice;
+
+    humanChoice = prompt("Choice");
+    return humanChoice.toLowerCase();;
+
 }
 
-else{
-    compChoice = "Scissor";
+function playRound(compChoice, humanChoice) {
+
+    if (humanChoice === compChoice) {
+        console.log("It's a tie, Reload and try again!", "Good Luck");
+        return;
+    }
+    if (
+        (humanChoice === "rock" && compChoice === "scissor") || (humanChoice === "paper" && compChoice === "rock") || (humanChoice === "scissor" && compChoice === "paper")
+
+    ) {
+        humanScore++;
+        console.log("Congratulations, you won", "Relaod to try again");
+    }
+
+    else{
+        computerScore++;
+        console.log("You Lose", "Reload to try again");
+    }
 }
 
-userChoice = prompt("Choice");
+let compChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
 
-console.log(numberRandom);
+playRound(compChoice,humanChoice);
+
+
+//console.log(numberRandom);
 console.log(compChoice);
-console.log(userChoice);
+console.log(humanChoice);
